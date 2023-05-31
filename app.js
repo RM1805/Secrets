8d219e1f43fe88d6b9b0a47ad7b5bc0d66f74956
Rishi <rishimishra18052001@gmail.com>
2023-05-31 11:24:00 +0700

    fourth commit

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://share-secrets-pirj.onrender.com/auth/google/secrets"
  },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -195,8 +195,4 @@ req.login(user, function(err){
 app.listen(process.env.PORT || 3000, function(){
     console.log("server started on port 3000");
 });
-
-
-
-
 
